c5ede66e3a9b1d7f43e4615245bc0757bc591040
fazri-png <fazrixganz@gmail.com>
2026-01-07 20:41:03 +0700

    project day one

---
+++ script.py
@@ -0,0 +1,9 @@
+print("hallo, welcome.")
+
+name = input("what is your name?")
+age = int(input('how old are you?'))
+favorite_food = input('what is your favorite food?')
+
+print("-----------------------")
+print("hallo",name,",""your are", age, "years old, and your favorite food", favorite_food,".")
+print("nice to meet you")
