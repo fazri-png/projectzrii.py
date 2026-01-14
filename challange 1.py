e5810365bac78e5fc3af3298a626c8f958c5962f
fazri-png <fazrixganz@gmail.com>
2026-01-14 20:27:01 +0700

    Add files via upload

---
+++ challange 1.py
@@ -0,0 +1,4 @@
+print('my name is axel lotl.')
+print('i m 12 years old.')
+print('i love in jakarta.')
+print('i love progamming in python.')
---
+++ challange 2.py
@@ -0,0 +1,8 @@
+player_hp = 100
+potion = 25
+damage = 40
+
+player_hp =player_hp + potion
+print(player_hp)
+player_hp = player_hp - damage
+print(player_hp)
